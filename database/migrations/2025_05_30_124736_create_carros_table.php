207158071d8992ddf963d0dce70841a1266f417f
Ravi <ravi@example.com>
2025-05-30 09:59:27 -0300

    feat: form criacao

---
+++ database/migrations/2025_05_30_124736_create_carros_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('modelo');
+            $table->text('marca');
+            $table->integer('ano');
+            $table->text('status');
+            $table->text('imagem');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carros');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+
+Route::view('carros.create', 'carros.create')
+    ->middleware(['auth'])
+    ->name('carros.create');
+
+
+require __DIR__ . '/auth.php';
